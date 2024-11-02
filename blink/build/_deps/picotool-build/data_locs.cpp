3500c757d3697c5b48a23db71c476e81c6afb86d
Aleks <alsa0622@hotmail.com>
2024-11-02 11:19:58 -0400

    Add .gitignore and remove build artifacts

--- blink/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/aleks/Documents/freertos-pico/blink/build/_deps/picotool/"
-};
--- blink/main.c
+++ blink/main.c
@@ -2,20 +2,34 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#define LED_PIN 15 // GPIO pin for the external LED
+#define LED_PIN1 14 // GPIO pin for the external LED
+#define LED_PIN2 15 // Blinking delay in milliseconds
 
-void vBlinkTask() {
+void vBlinkTask1() {
    for (;;) {
-      gpio_put(LED_PIN, 1);
+      gpio_put(LED_PIN1, 1);
       vTaskDelay(1000);
-      gpio_put(LED_PIN, 0);
+      gpio_put(LED_PIN1, 0);
       vTaskDelay(1000);
    }
 }
 
+void vBlinkTask2() {
+   for (;;) {
+      gpio_put(LED_PIN2, 1);
+      vTaskDelay(500);
+      gpio_put(LED_PIN2, 0);
+      vTaskDelay(500);
+   }
+}
+
+
 void main() {
-   gpio_init(LED_PIN);
-   gpio_set_dir(LED_PIN, GPIO_OUT);
-   xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
+   gpio_init(LED_PIN1);
+   gpio_set_dir(LED_PIN1, GPIO_OUT);
+   gpio_init(LED_PIN2);
+   gpio_set_dir(LED_PIN2, GPIO_OUT);
+   xTaskCreate(vBlinkTask1, "Blink Task 1", 128, NULL, 1, NULL);
+   xTaskCreate(vBlinkTask2, "Blink Task 2", 128, NULL, 1, NULL);
    vTaskStartScheduler();
 }
